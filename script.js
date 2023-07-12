3b0f99a2e1d580dd300e4e4d279ee0e87c55ced1
KarthikaMudhiraj <139123256+KarthikaMudhiraj@users.noreply.github.com>
2023-07-12 12:07:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,12 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-   
+   var typed = new Typed(".typing-2", {
+        strings: ["WEB DEVELOPER"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
